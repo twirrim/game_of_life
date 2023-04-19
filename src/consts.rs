3d2d45e446c2ff95436ad9203cd93f1b941a3b86
Paul Graydon <paulgraydon@hotmail.com>
2023-04-19 14:41:16 +0000

    Add CLI, remove decay.
    Decay was interesting, but I felt it mostly made the display messy.
    Maybe I'll resurrect it later on.

--- src/consts.rs
+++
@@ -1,6 +0,0 @@
-// This is a bad idea!  All of these should really be specified by CLI arguments!
-//3840x2160
-pub const WIDTH: usize = 3840;
-pub const HEIGHT: usize = 2160;
-pub const OUTPUT_PATH: &str = "./output";
-pub const FRAMES: u32 = 9000;
--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,39 @@ use std::sync::mpsc::sync_channel;
 use std::thread;
 use std::thread::available_parallelism;
 
+use clap::Parser;
 use indicatif::{ProgressBar, ProgressStyle};
 use rayon::iter::ParallelBridge;
 use rayon::prelude::*;
 use ril::prelude::*;
 
-use gol::consts::{FRAMES, HEIGHT, OUTPUT_PATH, WIDTH};
 use gol::{initialise, process_frame};
 
+#[derive(Parser, Debug)]
+struct Cli {
+    #[arg(short, long)]
+    width: isize,
+    #[arg(short, long)]
+    height: isize,
+    #[arg(short, long)]
+    frames: usize,
+    #[arg(short, long)]
+    output: String,
+}
+
 fn main() {
+    let args = Cli::parse();
     let (tx, rx) = sync_channel(available_parallelism().unwrap().get());
 
     let simulation = thread::spawn(move || {
         println!("Creating initial colony");
-        let starting_cells = ((WIDTH as f32 * HEIGHT as f32) * 0.5) as u32;
+        let starting_cells = ((args.width as f32 * args.height as f32) * 0.5) as usize;
         println!("Starting with {:} cells", starting_cells);
         println!("Randomising starting cells");
 
-        let mut cells = initialise(starting_cells, WIDTH, HEIGHT);
+        let mut cells = initialise(starting_cells, args.width, args.height);
         println!("Colony initialised");
-        for frame in 0..FRAMES {
+        for frame in 0..args.frames {
             process_frame(&mut cells);
             tx.send((frame, cells.clone())).unwrap();
         }
@@ -34,29 +47,25 @@ fn main() {
     )
     .unwrap();
 
-    fs::create_dir_all(OUTPUT_PATH).unwrap();
-    let pb = ProgressBar::new(FRAMES as u64);
+    fs::create_dir_all(&args.output).unwrap();
+    let pb = ProgressBar::new(args.frames as u64);
     pb.set_style(style);
 
     let collector = rx.into_iter().par_bridge();
 
     collector.for_each(|(frame, colony)| {
-        let mut current_image = Image::new(WIDTH as u32, HEIGHT as u32, Rgb::black());
+        let mut current_image = Image::new(args.width as u32, args.height as u32, Rgb::black());
         let mut live_cells = 0;
         for (x, row) in colony.cells.iter().enumerate() {
             for (y, cell) in row.iter().enumerate() {
                 if cell.alive {
                     live_cells += 1;
+                    current_image.set_pixel(x as u32, y as u32, Rgb::white());
                 };
-                current_image.set_pixel(
-                    x as u32,
-                    y as u32,
-                    Rgb::new(cell.life_left, cell.life_left, cell.life_left),
-                );
             }
         }
         current_image
-            .save_inferred(format!("{OUTPUT_PATH}/{:08}.png", frame))
+            .save_inferred(format!("{}/{:08}.png", &args.output, frame))
             .unwrap();
         pb.inc(1);
         pb.set_message(format!("Live cells: {}", live_cells));
